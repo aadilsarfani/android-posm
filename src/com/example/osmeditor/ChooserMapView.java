07d38483e418eba46564de160c166fcdbf43415f
Aadil Sarfani <aadilsarfani@gmail.com>
2013-11-03 21:04:22 -0600

    pin added

--- src/com/example/osmeditor/ChooserMapView.java
+++ src/com/example/osmeditor/ChooserMapView.java
@@ -5,27 +5,53 @@ import org.osmdroid.views.MapView;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.BitmapFactory.Options;
 import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Rect;
 import android.util.AttributeSet;
+import android.util.Log;
 
 public class ChooserMapView extends MapView{
 	
-	ChooserMapView(Context context, AttributeSet attrs) {
+	private Bitmap pin;
+	private Paint paint;
+	private Rect r;
+	
+	public ChooserMapView(Context context, AttributeSet attrs) {
 		super(context, attrs);
+		pin = BitmapFactory.decodeResource(getResources(), R.drawable.pin);
+		paint = new Paint();
+		paint.setARGB(150,150,150,150);
+		paint.setStrokeWidth(15);
+		r = new Rect();
 	}
 	
-	/*protected void onDraw(Canvas canvas) {
+	@Override
+	protected void onMeasure(int w, int h) {
+		super.onMeasure(w, h);
+		Log.v("aadil", "h = "+this.getHeight());
+		Log.v("aadil", "w = "+this.getWidth());
+		Log.v("aadil", "hh = "+this.getMeasuredHeight());
+		Log.v("aadil", "ww = "+this.getMeasuredWidth());
+	}
+	
+	@Override
+	protected void onDraw(Canvas canvas) {
 		super.onDraw(canvas);
 		int w = canvas.getWidth();
+		Log.v("aadil", "width = "+w);
 		int h = canvas.getHeight();
-		Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.pin);
-		int pin_w = w/25;
-		int pin_h = pin_w * bitmap.getHeight() / bitmap.getWidth();
-		bitmap = Bitmap.createScaledBitmap(bitmap,  pin_w,  pin_h, true);
+		Log.v("aadil", "height = "+h);
+		int pin_w = w/20;
+		int pin_h = pin_w * pin.getHeight() / pin.getWidth();
 		int x = (w - pin_w)/2;
 		int y = h/2 - pin_h;
-		canvas.drawBitmap(bitmap, x, y, null);
-	}*/
-
+		r.set(x, y, x+pin_w, y+pin_h);
+		canvas.drawBitmap(pin, null, r, paint);
+		canvas.drawBitmap(pin, x, y, paint);
+		canvas.drawRect(10,  10,  100,  100, paint);
+		canvas.drawRect(0,0,10,10,paint);
+		Log.v("aadil", "aadil");
+	}
 }
--- src/com/example/osmeditor/LocationChooser.java
+++ src/com/example/osmeditor/LocationChooser.java
@@ -1,10 +1,15 @@
 package com.example.osmeditor;
 
-import org.osmdroid.views.MapView;
-
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.Config;
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.widget.ImageView;
 
 public class LocationChooser extends Activity {
 
@@ -13,11 +18,21 @@ public class LocationChooser extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_location_chooser);
 		
-		
 		ChooserMapView map = (ChooserMapView) findViewById(R.id.chooser_mapview);
 		map.setBuiltInZoomControls(true);
 		map.setMultiTouchControls(true);
-		
+		Log.v("aadil", "zzz");
+
+//		ImageView iv = (ImageView) findViewById(R.id.pin_image);
+//		Bitmap b = ((BitmapDrawable)getResources().getDrawable(R.drawable.pin)).getBitmap();
+//		b = b.copy(Config.ARGB_8888, true);
+//		int x = b.getWidth();
+//		int y = b.getHeight();
+//		for (int i = 0; i < x; i++)
+//			for (int j = 0; j < y; j++)
+//				if (b.getPixel(i, j) == Color.WHITE)
+//					b.setPixel(i, j, Color.TRANSPARENT);
+//		iv.setImageBitmap(b);
 	}
 
 	@Override
